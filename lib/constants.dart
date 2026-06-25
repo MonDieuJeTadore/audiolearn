5a041d3c241fded82a8c50c6be1986fd85ec0858
Jean-Pierre Schnyder <jp.schnyder@gmail.com>
2026-06-25 11:56:23 +0200

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,7 +4,7 @@ import 'package:intl/intl.dart';
 const String kApplicationName = "AudioLearn";
 const String kApplicationNameForAboutView =
     "Audio Learn"; // Enables better readability
-const String kApplicationVersion = "3.4.91";
+const String kApplicationVersion = "3.5.0";
 
 enum PopupMenuButtonType {
   openSortFilterAudioDialog,

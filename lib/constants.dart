67cd8cd8e8503bf836cb59add7b5903b9ceb6a75
Jean-Pierre Schnyder <jp.schnyder@gmail.com>
2026-07-07 20:09:39 +0200

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,7 +4,7 @@ import 'package:intl/intl.dart';
 const String kApplicationName = "AudioLearn";
 const String kApplicationNameForAboutView =
     "Audio Learn"; // Enables better readability
-const String kApplicationVersion = "3.5.32";
+const String kApplicationVersion = "3.5.4";
 
 enum PopupMenuButtonType {
   openSortFilterAudioDialog,
@@ -30,6 +30,8 @@ enum AudioPopupMenuAction {
   renameAudioFile,
   moveAudioToPlaylist,
   copyAudioToPlaylist,
+  definePlayableOnlyWeekDays,
+  definePlayableOnlyMonthDays,
   deleteAudio,
   deleteAudioFromPlaylistAswell,
   addAudioPicture,

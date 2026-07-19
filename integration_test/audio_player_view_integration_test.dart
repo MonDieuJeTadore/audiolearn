764274be7343c4ae00980579dabe270f3cca2abc
Jean-Pierre Schnyder <jp.schnyder@gmail.com>
2026-07-19 17:10:29 +0200

    Update audio_player_view_integration_test.dart

--- integration_test/audio_player_view_integration_test.dart
+++ integration_test/audio_player_view_integration_test.dart
@@ -11808,7 +11808,7 @@ void main() {
         );
       });
     });
-    group('Tests executable only with audioplayers 6.4.0 or higher', () {
+    group('With audioplayers 6.4.0 or higher executable only', () {
       testWidgets(
           '''Verify that after typing once on the decrease end position comment button in very short
            comment (0:00 to 0:04) the comment plays till 0:03 seconds and not later.''',

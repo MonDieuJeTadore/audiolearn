9e56ede9288016d28c91ffcb4c5e5ffe7b9c0f5a
Jean-Pierre Schnyder <jp.schnyder@gmail.com>
2026-09-03 09:49:15 +0200

    not yet ok

--- android/app/src/main/kotlin/com/audiolearn/MainActivity.kt
+++ android/app/src/main/kotlin/com/audiolearn/MainActivity.kt
@@ -1,5 +1,32 @@
 package com.audiolearn
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    private val channelName = "audiolearn/yt_dlp"
+
+    override fun configureFlutterEngine(
+        flutterEngine: FlutterEngine
+    ) {
+        super.configureFlutterEngine(flutterEngine)
+
+        MethodChannel(
+            flutterEngine.dartExecutor.binaryMessenger,
+            channelName
+        ).setMethodCallHandler { call, result ->
+            when (call.method) {
+                "downloadAudio" -> {
+                    // yt-dlp Android download
+                }
+
+                "getPlaylistVideos" -> {
+                    // yt-dlp playlist extraction
+                }
+
+                else -> result.notImplemented()
+            }
+        }
+    }
+}

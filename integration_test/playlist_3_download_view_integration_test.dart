6033597d8fac3eae05793f3c61a09190b6da5c6f
Jean-Pierre Schnyder <jp.schnyder@gmail.com>
2026-09-20 14:21:03 +0200

    1 tr ok

--- integration_test/playlist_3_download_view_integration_test.dart
+++ integration_test/playlist_3_download_view_integration_test.dart
@@ -55,8 +55,435 @@ void main() {
 
   const String testPlaylistDir =
       '$kApplicationPathWindowsTest\\audio_learn_new_youtube_playlist_test';
+  group('AudioDownloadVM using CustomMockYoutubeExplode Tests', () {
+    late CustomMockYoutubeExplode mockYoutubeExplode;
 
-  group('Add or delete Youtube or local Playlist tests', () {
-    
+    setUp(() async {
+      mockYoutubeExplode = CustomMockYoutubeExplode();
+    });
+
+    testWidgets(
+        'Download single video audio in playlist already containing the audio',
+        (WidgetTester tester) async {
+      // Purge the test playlist directory if it exists so that the
+      // playlist list is empty
+      DirUtil.deleteFilesInDirAndSubDirs(
+        rootPath: kApplicationPathWindowsTest,
+      );
+
+      // Copy the test initial audio data to the app dir
+      DirUtil.copyFilesFromDirAndSubDirsToDirectory(
+        sourceRootPath:
+            "$kDownloadAppTestSavedDataDir${path.separator}copy_move_audio_integr_test_data",
+        destinationRootPath: kApplicationPathWindowsTest,
+      );
+
+      final SettingsDataService settingsDataService = SettingsDataService(
+        isTest: true,
+      );
+
+      // Load the settings from the json file. This is necessary
+      // otherwise the ordered playlist titles will remain empty
+      // and the playlist list will not be filled with the
+      // playlists available in the download app test dir
+      await settingsDataService.loadSettingsFromFile(
+          settingsJsonPathFileName:
+              "$kApplicationPathWindowsTest${path.separator}$kSettingsFileName");
+
+      // Since we have to use a mock AudioDownloadVM to add the
+      // youtube playlist, we can not use app.main() to start the
+      // app because app.main() uses the real AudioDownloadVM
+      // and we don't want to make the main.dart file dependent
+      // of a mock class. So we have to start the app by hand.
+
+      WarningMessageVM warningMessageVM = WarningMessageVM();
+      // MockAudioDownloadVM mockAudioDownloadVM = MockAudioDownloadVM(
+      //   warningMessageVM: warningMessageVM,
+      //
+      // );
+      // mockAudioDownloadVM.youtubePlaylistTitle = youtubeNewPlaylistTitle;
+
+      AudioDownloadVM audioDownloadVM = AudioDownloadVM(
+        warningMessageVM: warningMessageVM,
+        settingsDataService: settingsDataService,
+      );
+
+      audioDownloadVM.youtubeExplode = mockYoutubeExplode;
+
+      PlaylistListVM playlistListVM = PlaylistListVM(
+        warningMessageVM: warningMessageVM,
+        audioDownloadVM: audioDownloadVM,
+        commentVM: CommentVM(isTest: true),
+        pictureVM: PictureVM(
+          settingsDataService: settingsDataService,
+        ),
+        settingsDataService: settingsDataService,
+      );
+
+      // calling getUpToDateSelectablePlaylists() loads all the
+      // playlist json files from the app dir and so enables
+      // playlistListVM to know which playlists are
+      // selected and which are not
+      playlistListVM.getUpToDateSelectablePlaylists();
+
+      AudioPlayerVM audioPlayerVM = AudioPlayerVM(
+        settingsDataService: settingsDataService,
+        playlistListVM: playlistListVM,
+        commentVM: CommentVM(isTest: true),
+      );
+
+      DateFormatVM dateFormatVM = DateFormatVM(
+        settingsDataService: settingsDataService,
+      );
+
+      await IntegrationTestUtil
+          .launchIntegrTestAppEnablingInternetAccessWithMock(
+        tester: tester,
+        audioDownloadVM: audioDownloadVM,
+        settingsDataService: settingsDataService,
+        playlistListVM: playlistListVM,
+        warningMessageVM: warningMessageVM,
+        audioPlayerVM: audioPlayerVM,
+        dateFormatVM: dateFormatVM,
+        forcedLocale: const Locale('en'),
+      );
+
+      const String youtubeAudioSourceAndTargetPlaylistTitle =
+          'audio_learn_test_download_2_small_videos';
+      const String downloadedSingleVideoAudioTitle =
+          'audio learn test short video one';
+
+      // Copy the URL of source playlist audio file which wiil
+      // be downloaded to the same (source) playlist, causing a
+      // warning error to be displayed ...
+
+      // Tap the 'Toggle List' button to show the list of playlist's.
+      await tester.tap(find.byKey(const Key('playlist_toggle_button')));
+      await tester.pumpAndSettle();
+
+      // Now we want to tap the popup menu of the Audio ListTile
+      // "audio learn test short video one"
+
+      // First, find the Audio sublist ListTile Text widget
+      final Finder sourceAudioListTileTextWidgetFinder =
+          find.text(downloadedSingleVideoAudioTitle);
+
+      // Then obtain the Audio ListTile widget enclosing the Text widget by
+      // finding its ancestor
+      final Finder sourceAudioListTileWidgetFinder = find.ancestor(
+        of: sourceAudioListTileTextWidgetFinder,
+        matching: find.byType(ListTile),
+      );
+
+      // Now find the leading menu icon button of the Audio ListTile and tap
+      // on it
+      final Finder sourceAudioListTileLeadingMenuIconButton = find.descendant(
+        of: sourceAudioListTileWidgetFinder,
+        matching: find.byIcon(Icons.menu),
+      );
+
+      // Tap the leading menu icon button to open the popup menu
+      await tester.tap(sourceAudioListTileLeadingMenuIconButton);
+      await tester.pumpAndSettle();
+
+      // Now find the copy video URL popup menu item and tap on it
+      final Finder popupCopyVideoUrlMenuItem =
+          find.byKey(const Key("popup_copy_youtube_video_url"));
+
+      await tester.tap(popupCopyVideoUrlMenuItem);
+      await tester.pumpAndSettle();
+
+      ClipboardData? clipboardData =
+          await Clipboard.getData(Clipboard.kTextPlain);
+      String singleVideoToDownloadUrl = clipboardData?.text ?? '';
+
+      // Enter the single video URL to download into the url text
+      // field
+      await tester.enterText(
+        find.byKey(
+          const Key('youtubeUrlOrSearchTextField'),
+        ),
+        singleVideoToDownloadUrl,
+      );
+      await tester.pumpAndSettle();
+
+      // Ensure the url text field contains the entered url
+      TextField urlTextField = tester.widget(find.byKey(
+        const Key('youtubeUrlOrSearchTextField'),
+      ));
+      expect(urlTextField.controller!.text, singleVideoToDownloadUrl);
+
+      // Open the target playlist selection dialog by tapping the
+      // download single video button
+      await tester.tap(find.byKey(const Key('downloadSingleVideoButton')));
+      await tester.pumpAndSettle();
+
+      // Ensure the dialog is shown
+      expect(find.byType(AlertDialog), findsOneWidget);
+
+      // Check the value of the select one playlist AlertDialog
+      // dialog title
+      Text alertDialogTitle = tester
+          .widget(find.byKey(const Key('playlistOneSelectableDialogTitleKey')));
+      expect(alertDialogTitle.data, 'Select a Playlist');
+
+      // Find the RadioListTile target playlist to which the audio
+      // will be downloaded
+
+      final Finder radioListTile = find
+          .ancestor(
+            of: find.text(youtubeAudioSourceAndTargetPlaylistTitle),
+            matching: find.byType(ListTile),
+          )
+          .last;
+
+      // Tap the target playlist RadioListTile to select it
+      await tester.tap(radioListTile);
+      await tester.pumpAndSettle();
+
+      // Now find the confirm button and tap on it
+      await tester.tap(find.byKey(const Key('confirmButton')));
+      await tester.pumpAndSettle();
+
+      // Now verifying the confirm warning dialog message
+
+      // Check the value of the select one playlist
+      // confirmation dialog title
+      Text confirmationDialogTitle =
+          tester.widget(find.byKey(const Key('confirmationDialogTitleKey')));
+      expect(confirmationDialogTitle.data, 'CONFIRMATION');
+
+      final Text confirmationDialogMessageTextWidget = tester
+          .widget<Text>(find.byKey(const Key('confirmationDialogMessageKey')));
+
+      expect(confirmationDialogMessageTextWidget.data,
+          'Confirm target playlist "$youtubeAudioSourceAndTargetPlaylistTitle" for downloading single video audio in spoken quality.');
+
+      // Now find the ok button of the confirm warning dialog
+      // and tap on it
+      await tester.tap(find.byKey(const Key('okButtonKey')));
+      await tester.pumpAndSettle();
+
+      await IntegrationTestUtil.verifyAndCloseWarningDialog(
+        tester: tester,
+        warningDialogMessage:
+            'Audio "$downloadedSingleVideoAudioTitle" is contained in file "230628-033811-audio learn test short video one 23-06-10.mp3" present in the target playlist "$youtubeAudioSourceAndTargetPlaylistTitle" directory and so won\'t be redownloaded.',
+        isWarningConfirming: false,
+      );
+
+      // Ensure the URL TextField containing the invalid single
+      // video URL was not emptied
+      urlTextField = tester.widget(find.byKey(
+        const Key('youtubeUrlOrSearchTextField'),
+      ));
+      expect(urlTextField.controller!.text, singleVideoToDownloadUrl);
+
+      // Purge the test playlist directory so that the created test
+      // files are not uploaded to GitHub
+      DirUtil.deleteFilesInDirAndSubDirs(
+        rootPath: kApplicationPathWindowsTest,
+      );
+    });
+    testWidgets(
+        'Download single video audio in playlist not containing the audio',
+        (WidgetTester tester) async {
+      // Purge the test playlist directory if it exists so that the
+      // playlist list is empty
+      DirUtil.deleteFilesInDirAndSubDirs(
+        rootPath: kApplicationPathWindowsTest,
+      );
+
+      // Copy the test initial audio data to the app dir
+      DirUtil.copyFilesFromDirAndSubDirsToDirectory(
+        sourceRootPath:
+            "$kDownloadAppTestSavedDataDir${path.separator}copy_move_audio_integr_test_data",
+        destinationRootPath: kApplicationPathWindowsTest,
+      );
+
+      final SettingsDataService settingsDataService = SettingsDataService(
+        isTest: true,
+      );
+
+      // Load the settings from the json file. This is necessary
+      // otherwise the ordered playlist titles will remain empty
+      // and the playlist list will not be filled with the
+      // playlists available in the download app test dir
+      await settingsDataService.loadSettingsFromFile(
+          settingsJsonPathFileName:
+              "$kApplicationPathWindowsTest${path.separator}$kSettingsFileName");
+
+      // Since we have to use a mock AudioDownloadVM to add the
+      // youtube playlist, we can not use app.main() to start the
+      // app because app.main() uses the real AudioDownloadVM
+      // and we don't want to make the main.dart file dependent
+      // of a mock class. So we have to start the app by hand.
+
+      WarningMessageVM warningMessageVM = WarningMessageVM();
+      // MockAudioDownloadVM mockAudioDownloadVM = MockAudioDownloadVM(
+      //   warningMessageVM: warningMessageVM,
+      //
+      // );
+      // mockAudioDownloadVM.youtubePlaylistTitle = youtubeNewPlaylistTitle;
+
+      AudioDownloadVM audioDownloadVM = AudioDownloadVM(
+        warningMessageVM: warningMessageVM,
+        settingsDataService: settingsDataService,
+      );
+
+      audioDownloadVM.youtubeExplode = mockYoutubeExplode;
+
+      PlaylistListVM playlistListVM = PlaylistListVM(
+        warningMessageVM: warningMessageVM,
+        audioDownloadVM: audioDownloadVM,
+        commentVM: CommentVM(isTest: true),
+        pictureVM: PictureVM(
+          settingsDataService: settingsDataService,
+        ),
+        settingsDataService: settingsDataService,
+      );
+
+      // calling getUpToDateSelectablePlaylists() loads all the
+      // playlist json files from the app dir and so enables
+      // playlistListVM to know which playlists are
+      // selected and which are not
+      playlistListVM.getUpToDateSelectablePlaylists();
+
+      AudioPlayerVM audioPlayerVM = AudioPlayerVM(
+        settingsDataService: settingsDataService,
+        playlistListVM: playlistListVM,
+        commentVM: CommentVM(isTest: true),
+      );
+
+      DateFormatVM dateFormatVM = DateFormatVM(
+        settingsDataService: settingsDataService,
+      );
+
+      await IntegrationTestUtil
+          .launchIntegrTestAppEnablingInternetAccessWithMock(
+        tester: tester,
+        audioDownloadVM: audioDownloadVM,
+        settingsDataService: settingsDataService,
+        playlistListVM: playlistListVM,
+        warningMessageVM: warningMessageVM,
+        audioPlayerVM: audioPlayerVM,
+        dateFormatVM: dateFormatVM,
+        forcedLocale: const Locale('en'),
+      );
+
+      const String localAudioTargetPlaylistTitle = 'local_3';
+      const String downloadedSingleVideoAudioTitle =
+          'audio learn test short video one';
+
+      // Copy the URL of source playlist audio file which wiil
+      // be downloaded to the same (source) playlist, causing a
+      // warning error to be displayed ...
+
+      // Tap the 'Toggle List' button to show the list of playlist's.
+      await tester.tap(find.byKey(const Key('playlist_toggle_button')));
+      await tester.pumpAndSettle();
+
+      // Now we want to tap the popup menu of the Audio ListTile
+      // "audio learn test short video one"
+
+      // First, find the Audio sublist ListTile Text widget
+      final Finder sourceAudioListTileTextWidgetFinder =
+          find.text(downloadedSingleVideoAudioTitle);
+
+      // Then obtain the Audio ListTile widget enclosing the Text widget by
+      // finding its ancestor
+      final Finder sourceAudioListTileWidgetFinder = find.ancestor(
+        of: sourceAudioListTileTextWidgetFinder,
+        matching: find.byType(ListTile),
+      );
+
+      // Now find the leading menu icon button of the Audio ListTile and tap
+      // on it
+      final Finder sourceAudioListTileLeadingMenuIconButton = find.descendant(
+        of: sourceAudioListTileWidgetFinder,
+        matching: find.byIcon(Icons.menu),
+      );
+
+      // Tap the leading menu icon button to open the popup menu
+      await tester.tap(sourceAudioListTileLeadingMenuIconButton);
+      await tester.pumpAndSettle();
+
+      // Now find the copy video URL popup menu item and tap on it
+      final Finder popupCopyVideoUrlMenuItem =
+          find.byKey(const Key("popup_copy_youtube_video_url"));
+
+      await tester.tap(popupCopyVideoUrlMenuItem);
+      await tester.pumpAndSettle();
+
+      ClipboardData? clipboardData =
+          await Clipboard.getData(Clipboard.kTextPlain);
+      String singleVideoToDownloadUrl = clipboardData?.text ?? '';
+
+      // Enter the single video URL to download into the url text
+      // field
+      await tester.enterText(
+        find.byKey(
+          const Key('youtubeUrlOrSearchTextField'),
+        ),
+        singleVideoToDownloadUrl,
+      );
+      await tester.pumpAndSettle();
+
+      // Ensure the url text field contains the entered url
+      TextField urlTextField = tester.widget(find.byKey(
+        const Key('youtubeUrlOrSearchTextField'),
+      ));
+      expect(urlTextField.controller!.text, singleVideoToDownloadUrl);
+
+      // Open the target playlist selection dialog by tapping the
+      // download single video button
+      await tester.tap(find.byKey(const Key('downloadSingleVideoButton')));
+      await tester.pumpAndSettle();
+
+      // Ensure the dialog is shown
+      expect(find.byType(AlertDialog), findsOneWidget);
+
+      // Check the value of the select one playlist AlertDialog
+      // dialog title
+      Text alertDialogTitle = tester
+          .widget(find.byKey(const Key('playlistOneSelectableDialogTitleKey')));
+      expect(alertDialogTitle.data, 'Select a Playlist');
+
+      // Find the RadioListTile target playlist to which the audio
+      // will be downloaded
+
+      final Finder radioListTile = find
+          .ancestor(
+            of: find.text(localAudioTargetPlaylistTitle),
+            matching: find.byType(ListTile),
+          )
+          .last;
+
+      // Tap the target playlist RadioListTile to select it
+      await tester.tap(radioListTile);
+      await tester.pumpAndSettle();
+
+      // Now find the confirm button and tap on it
+      await tester.tap(find.byKey(const Key('confirmButton')));
+      await tester.pumpAndSettle();
+
+      // Now find the ok button of the confirm warning dialog
+      // and tap on it
+      await tester.tap(find.byKey(const Key('okButtonKey')));
+      await tester.pumpAndSettle();
+
+      // Ensure the URL TextField containing the invalid single
+      // video URL was not emptied
+      urlTextField = tester.widget(find.byKey(
+        const Key('youtubeUrlOrSearchTextField'),
+      ));
+      expect(urlTextField.controller!.text, singleVideoToDownloadUrl);
+
+      // Purge the test playlist directory so that the created test
+      // files are not uploaded to GitHub
+      DirUtil.deleteFilesInDirAndSubDirs(
+        rootPath: kApplicationPathWindowsTest,
+      );
+    });
   });
+
 }

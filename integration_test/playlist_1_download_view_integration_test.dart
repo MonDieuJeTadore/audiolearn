f6548348fd08ff13c18be5f3c8d5c9e5c1951d27
Jean-Pierre Schnyder <jp.schnyder@gmail.com>
2026-09-21 08:15:49 +0200

    cosmetic

--- integration_test/playlist_1_download_view_integration_test.dart
+++ integration_test/playlist_1_download_view_integration_test.dart
@@ -6,7 +6,6 @@ import 'package:audiolearn/viewmodels/picture_vm.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-import 'package:intl/intl.dart';
 import 'package:path/path.dart' as path;
 
 import 'package:audiolearn/viewmodels/audio_player_vm.dart';
